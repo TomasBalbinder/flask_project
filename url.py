2264e3dc244625826dbfdf8fe511f28ce53d3e82
Tomik <tomas>
2020-11-12 11:06:32 +0000

    create URL address

---
+++ url.py
@@ -0,0 +1,10 @@
+from app import *
+
+from flask import url_for
+
+
+with app.test_request_context():
+
+    print(url_for("text"))
+    print(url_for("admin"))
+    print(url_for("admin_name", name="Franta"))
